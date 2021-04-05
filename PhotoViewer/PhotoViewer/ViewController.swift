7addc3fd5bdc5da129c8ea329402f240252e290f
ihuettel <ian.huettel@gmail.com>
2021-04-04 23:48:18 -0500

    Added assets and changed ViewController to inherit from UITableViewController

---
+++ PhotoViewer/PhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by homebase on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
