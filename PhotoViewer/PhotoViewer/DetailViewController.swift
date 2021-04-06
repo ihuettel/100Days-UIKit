d322140226f70543ed96c3c4e820d569fb5c9e84
ihuettel <ian.huettel@gmail.com>
2021-04-06 01:05:43 -0500

    Last big commit, including wrap up challenges and some comments.

--- PhotoViewer/PhotoViewer/DetailViewController.swift
+++ PhotoViewer/PhotoViewer/DetailViewController.swift
@@ -9,15 +9,47 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    // Make a link to our Image View in Interface Builder, which will definitely always be here.
     @IBOutlet weak var displayImage: UIImageView!
     
+    // Declare our properties as optionals, as we're defining them after initialization
     var imageName: String?
+    var imageNum: Int?
+    var imageCount: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set the title to it's number relative to the number of images
+        // but if that fails, we just make it the name of the image
+        // which is inherently optional, so if that fails, that's okay too.
+        if let imageNum = imageNum, let imageCount = imageCount {
+            title = "\(imageNum) of \(imageCount)"
+        } else {
+            title = imageName
+        }
+        
+        // Make sure our large titles stay where they belong – not here
+        navigationItem.largeTitleDisplayMode = .never
+        
+        // Make sure we have an image that matches the name, then associate it.
         if let foundImage = imageName {
             displayImage.image = UIImage(named: foundImage)
         }
+    }
+    
+    
+    // Set up the ability to hide the Navigation Bar by tapping the screen
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    // Make sure we take away the ability to hide the Nav Bar when we want to leave
+    // ...or else weird things might happen...
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 
 }
--- PhotoViewer/PhotoViewer/ViewController.swift
+++ PhotoViewer/PhotoViewer/ViewController.swift
@@ -8,35 +8,67 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    // Create an array of strings to hold our picture names
     var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Define and design our title for the Table View Controller
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Declare a file manager to get our files
         let fm = FileManager.default
+        // Define the path to our resource directory
+        // This is force unwrapped, because if this fails,
+        // we have much bigger problems.
         let path = Bundle.main.resourcePath!
+        // Then use our file manager and path to actually get
+        // an array of strings of all the file names from
+        // the resource directory.
         let items = try! fm.contentsOfDirectory(atPath: path)
         
+        // Iterate through all the files and find the ones
+        // that actually have the prefix of our images
+        // to make sure we only get what we want.
         for item in items {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
             }
         }
+        
+        // Sort them so they appear in alphanumerical order
+        pictures.sort()
     }
     
+    // Display the correct number of cells in our Table View
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
     
+    // Display our cells using reusable cells, naming them
+    // according to the name we found for them in the resource path
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
-    
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Conditionally make sure our view controller actually
+        // can be found and interpreted as a DetailViewController
+        // (aka our custom UIViewController class)
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            
+            // If that's all good, define the 3 properties we left as optionals
             vc.imageName = pictures[indexPath.row]
+            vc.imageNum = indexPath.row + 1
+            vc.imageCount = pictures.count
+            
+            // Then we push it onto the view stack!
             navigationController?.pushViewController(vc, animated: true)
         }
         
