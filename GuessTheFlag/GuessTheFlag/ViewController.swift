086f46e18381814707522d93d7c9f8956c35ca4e
ihuettel <ian.huettel@gmail.com>
2021-04-07 00:38:00 -0500

    Basic UI laid out and title reflects correct answer – no button functionality yet.

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    
+    func askQuestion() {
+        
+        countries.shuffle()
+        
+        let correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+        
+        let buttons = [button1!, button2!, button3!]
+        
+        for (i, button) in buttons.enumerated() {
+            button.setImage(UIImage(named: countries[i]), for: .normal)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let buttons = [button1!, button2!, button3!]
+        
+        for button in buttons {
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        
+        askQuestion()
     }
 
 
